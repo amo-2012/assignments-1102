d175213cadf14c150c3a3b363b89f0c7e2f000e4
anno <amokeeffe@ymail.com>
2013-03-18 11:44:34 -0400

    testing subtractions

--- homework-9.1.js
+++ homework-9.1.js
@@ -1,40 +1,38 @@
-var assert = require("assert");
+var assert = require ("assert");
 
  /**
- * Homework 9  - Coding Dojos
+ * Homework 9  - Coding Dojos part 2
  * Four functions, each with 2 parameters:
  *   addition / subtraction / multiplication / division
  * 
  * testing positive and negative numbers - integer / float and zero.
  * 
- * testing addition:  
- * SUM = A +  B 
- *   2 | 1 |  1 
+ * testing subtraction:  
+ *   C = A -  B 
+ *   0 | 1 |  1 
  *   1 | 1 |  0
- *   0 | 1 | -1
- *   0 |-1 |  1
+ *   2 | 1 | -1
+ *  -2 |-1 |  1
  *  -1 |-1 |  0
- *  -2 |-1 | -1
- *   1 | 0 |  1
+ *   0 |-1 | -1
+ * - 1 | 0 |  1
  *   0 | 0 |  0
  *  -1 | 0 | -1
  */
 
 
-
- var add = function(a,b)  {
-        return (a+b);
-    }
+var sub = function (a,b)
+    {
+        return (a-b);
+    };
     
-assert.equal(2,add(1,1));  
-assert.equal(1,add(1,0));
-assert.equal(0,add(1,-1));
-assert.equal(0,add(-1,1));
-assert.equal(-1,add(-1,0));
-assert.equal(-2,add(-1,-1));
-assert.equal(1,add(0,1));
-assert.equal(0,add(0,0));
-assert.equal(-1,add(0,-1));
-
-
+assert.equal(0,sub(1,1));
+assert.equal(1,sub(1,0));
+assert.equal(2,sub(1,-1));
+assert.equal(-2,sub(-1,1));
+assert.equal(-1,sub(-1,0));
+assert.equal(0,sub(-1,-1));
+assert.equal(-1,sub(0,1));
+assert.equal(0,sub(0,0));
+assert.equal(1,sub(0,-1));
 
