86dcf7d1a6a1fe546bd1b3f79baadcdebdef6384
anno <amokeeffe@ymail.com>
2013-03-20 10:49:19 -0400

    multiplying

--- homework-9.1.js
+++ homework-9.1.js
@@ -1,38 +1,36 @@
-var assert = require ("assert");
+var assert = require("assert")
 
  /**
- * Homework 9  - Coding Dojos part 2
+ * Homework 9  - Coding Dojos part 3 Mulitply
  * Four functions, each with 2 parameters:
  *   addition / subtraction / multiplication / division
  * 
  * testing positive and negative numbers - integer / float and zero.
  * 
- * testing subtraction:  
- *   C = A -  B 
- *   0 | 1 |  1 
- *   1 | 1 |  0
- *   2 | 1 | -1
- *  -2 |-1 |  1
- *  -1 |-1 |  0
- *   0 |-1 | -1
- * - 1 | 0 |  1
+ * testing multiplication:  
+ *   C = A *  B 
+ *   0 | 1 |  0 
+ *   1 | 1 |  1
+ * - 1 | 1 | -1
+ * - 1 |-1 |  1
+ *   0 |-1 |  0
+ *   1 |-1 | -1
+ *   0 | 0 |  1
  *   0 | 0 |  0
- *  -1 | 0 | -1
+ *   0 | 0 | -1
  */
 
 
-var sub = function (a,b)
-    {
-        return (a-b);
-    };
-    
-assert.equal(0,sub(1,1));
-assert.equal(1,sub(1,0));
-assert.equal(2,sub(1,-1));
-assert.equal(-2,sub(-1,1));
-assert.equal(-1,sub(-1,0));
-assert.equal(0,sub(-1,-1));
-assert.equal(-1,sub(0,1));
-assert.equal(0,sub(0,0));
-assert.equal(1,sub(0,-1));
+var multi = function(a,b){
+    return (a*b);
+    }
 
+assert.equal(0,multi(1,0));
+assert.equal(1,multi(1,1));
+assert.equal(-1,multi(1,-1));
+assert.equal(-1,multi(-1,1));
+assert.equal(0,multi(-1,0));
+assert.equal(1,multi(-1,-1));
+assert.equal(0,multi(0,1));
+assert.equal(0,multi(0,0));
+assert.equal(0,multi(0,-1));
