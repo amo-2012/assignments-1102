952540502672069b91c25234d33249888475edd5
anno <amokeeffe@ymail.com>
2013-03-22 11:34:48 -0400

    dividing

--- homework-9.1.js
+++ homework-9.1.js
@@ -1,4 +1,4 @@
-var assert = require("assert")
+var assert = require ("assert");
 
  /**
  * Homework 9  - Coding Dojos part 3 Mulitply
@@ -7,30 +7,33 @@ var assert = require("assert")
  * 
  * testing positive and negative numbers - integer / float and zero.
  * 
- * testing multiplication:  
- *   C = A *  B 
- *   0 | 1 |  0 
- *   1 | 1 |  1
- * - 1 | 1 | -1
- * - 1 |-1 |  1
- *   0 |-1 |  0
- *   1 |-1 | -1
- *   0 | 0 |  1
- *   0 | 0 |  0
- *   0 | 0 | -1
+ * testing Division:  
+ *     C = A *  B 
+ *1    1 | 1 |  1 
+ *2    x | 1 |  0
+ *3  - 1 |-1 |  1
+ *4  - 1 | 1 | -1
+ *5    x |-1 |  0
+ *6    1 |-1 | -1
+ *7    0 | 0 |  1
+ *8    0 | 0 |  0
+ *9    0 | 0 | -1
+ *10   5 |20 | 4
  */
 
-
-var multi = function(a,b){
-    return (a*b);
+var div = function(a,b){
+    return a/b;
     }
+    
+assert.equal(1,div(1,1));
+//assert.equal(null,div(1,0));
+assert.equal(-1,div(-1,1));
+assert.equal(-1,div(1,-1));
+//assert.equal(null,div(-1,0));
+assert.equal(1,div(-1,-1));
+assert.equal(0,div(0,1));
+//assert.equal(0,div(0,0));
+assert.equal(0,div(0,-1));
+assert.equal(5,div(20,4));
 
-assert.equal(0,multi(1,0));
-assert.equal(1,multi(1,1));
-assert.equal(-1,multi(1,-1));
-assert.equal(-1,multi(-1,1));
-assert.equal(0,multi(-1,0));
-assert.equal(1,multi(-1,-1));
-assert.equal(0,multi(0,1));
-assert.equal(0,multi(0,0));
-assert.equal(0,multi(0,-1));
+
