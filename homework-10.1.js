36761ca81b8b776512f994b6668cb79042335dae
anno <amokeeffe@ymail.com>
2013-03-29 12:09:10 -0400

    subtraction

--- homework-10.1.js
+++ homework-10.1.js
@@ -72,3 +72,34 @@ assert.equal(0,add(-1,1));
 assert.equal(-2,add(-1,-1));
 assert.equal(-1,add(-1,0));
 
+
+
+
+
+//  subtraction
+/*
+ *  1 -  1 =  0
+ *  1 -  0 =  1
+ *  1 - -1 =  2
+ *  0 -  1 =  -1
+ *  0 -  0 =  0
+ *  0 - -1 =  1
+ * -1 -  1 =  -2
+ * -1 - -1 =  0
+ * -1 -  0 = -1
+*/
+
+
+var sub = function sub(a,b) {
+     return (a-b);
+}
+
+assert.equal(0,sub(1,1));
+assert.equal(1,sub(1,0));
+assert.equal(2,sub(1,-1));
+assert.equal(-1,sub(0,1));
+assert.equal(0,sub(0,0));
+assert.equal(1,sub(0,-1));
+assert.equal(-2,sub(-1,1));
+assert.equal(0,sub(-1,-1));
+assert.equal(-1,sub(-1,0));
