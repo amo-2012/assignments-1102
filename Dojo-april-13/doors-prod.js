5d70740570b89f09834f3ccad0af83f54030ec37
anno <amokeeffe@ymail.com>
2013-04-14 20:40:40 -0400

    commit 1

---
+++ Dojo-april-13/doors-prod.js
@@ -0,0 +1,20 @@
+/**
+ * 100 doors in a hallway.
+ * all are closed
+ * pass 1: toggle (open if closed ) every door
+ * pass 2: toggle every other door
+ * pass 3: toggle every 3rd door
+ * repeat 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+ 
+ module.exports = {
+      hallway: {
+           doors: [0,0,0,0,0,0,0,0,0,0],
+           toggle: []
+      }
+      }
---
+++ Dojo-april-13/doors-test.js
@@ -0,0 +1,11 @@
+var assert = require("assert");
+    hundredDoors = require('doors-Production.js');
+     
+
+
+var hallway = {
+    doors: [100]
+   };
+     
+     
+assert(hallway === true);
