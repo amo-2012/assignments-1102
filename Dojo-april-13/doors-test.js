a12424cfcc6fc5397c41305c9aa9338ff30c8ba8
anno <amokeeffe@ymail.com>
2013-04-19 11:58:07 -0400

    setting up hallway with doors

--- Dojo-april-13/doors-test.js
+++ Dojo-april-13/doors-test.js
@@ -1,11 +1,17 @@
 var assert = require("assert");
-    hundredDoors = require('doors-Production.js');
+    hundredDoors = require("./doors-prod.js");
      
 
-
+// starting with 10 doors to set up the pattern
 var hallway = {
-    doors: [100]
+    doors: [10]
    };
      
-     
-assert(hallway === true);
+ // hallway exists?    
+assert(hundredDoors.hallway);
+
+// doors in hallway?
+assert(hundredDoors.hallway.doors);
+
+// are there 100 doors?
+assert(hundredDoors.hallway.doors.length === 10);
